eda97b064724863d85584baf49dde98e20f576ae
alessandroberga <50765470+alessandroberga@users.noreply.github.com>
2025-05-06 07:23:12 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,36 @@
 module.exports = {
-    pathPrefix: '/',
-    siteMetadata: require('./site-metadata.json'),
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-source-data`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`,
-            },
-        },
-        `gatsby-plugin-sass`, // ✅ QUI: stringa semplice, non oggetto
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [`gatsby-remark-component`],
-            },
-        },
-        {
-            resolve: `gatsby-remark-page-creator`,
-            options: {
-                // Puoi aggiungere opzioni se servono
-            },
-        },
-        {
-            resolve: `@stackbit/gatsby-plugin-menus`,
-            options: {
-                sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`,
-                menus: require('./src/data/menus.json'),
-            },
-        },
-    ],
+  pathPrefix: '/',
+  siteMetadata: require('./site-metadata.json'),
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-source-data`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    `gatsby-plugin-sass`, // solo come stringa!
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-component`],
+      },
+    },
+    {
+      resolve: `gatsby-remark-page-creator`,
+      options: {
+        // eventualmente inserisci opzioni qui
+      },
+    },
+    {
+      resolve: `@stackbit/gatsby-plugin-menus`,
+      options: {
+        sourceUrlPath: `fields.url`,
+        pageContextProperty: `menus`,
+        menus: require('./src/data/menus.json'),
+      },
+    },
+  ],
 };
