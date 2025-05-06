e5d5421a88cacee9cd21f68e68b9857bd021b3c7
alessandroberga <50765470+alessandroberga@users.noreply.github.com>
2025-05-06 07:11:04 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,28 +11,26 @@ module.exports = {
                 path: `${__dirname}/src/pages`,
             },
         },
-        {
-           `gatsby-plugin-sass`,           
-        },
+        `gatsby-plugin-sass`, // ✅ QUI: stringa semplice, non oggetto
         {
             resolve: `gatsby-transformer-remark`,
             options: {
-                plugins: [`gatsby-remark-component`]
-            }
+                plugins: [`gatsby-remark-component`],
+            },
         },
         {
             resolve: `gatsby-remark-page-creator`,
             options: {
-                
-            }
+                // Puoi aggiungere opzioni se servono
+            },
         },
         {
             resolve: `@stackbit/gatsby-plugin-menus`,
             options: {
                 sourceUrlPath: `fields.url`,
                 pageContextProperty: `menus`,
-                menus: require('./src/data/menus.json'),
-            }
-        }
-    ]
+                menus: require('./src/data/menus.json`),
+            },
+        },
+    ],
 };
