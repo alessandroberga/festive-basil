a23036f0f600e9edf9830cdb625ea243a60295ad
alessandroberga <50765470+alessandroberga@users.noreply.github.com>
2026-03-19 12:30:54 +0100

    Refactor htmlToReact to use html-react-parser

--- src/utils/htmlToReact.js
+++ src/utils/htmlToReact.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import ReactHtmlParser, { convertNodeToElement } from 'react-html-parser';
-import ScriptTag from 'react-script-tag';
+import React, { useEffect, useRef } from 'react';
+import parse, { domToReact } from 'html-react-parser';
 import _ from 'lodash';
+
+function ScriptTag({ children, ...attrs }) {
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        const container = containerRef.current;
+        if (!container) return;
+
+        const script = document.createElement('script');
+        Object.entries(attrs).forEach(([key, value]) => {
+            if (value != null) {
+                script.setAttribute(key, value);
+            }
+        });
+
+        const content = React.Children.toArray(children).join('');
+        if (content) {
+            script.text = content;
+        }
+
+        container.appendChild(script);
+        return () => {
+            if (container.contains(script)) {
+                container.removeChild(script);
+            }
+        };
+    }, [attrs, children]);
+
+    return <span ref={containerRef} />;
+}
 
 export default function(html) {
     if (!html) {
         return null;
     }
-    return ReactHtmlParser(html, {
-        transform: (node, index) => {
-            if (node.type === 'script') {
-                console.log(node);
-                if (!_.isEmpty(node.children)) {
+
+    const options = {
+        replace: domNode => {
+            if (domNode.name === 'script') {
+                const attrs = domNode.attribs || {};
+                if (!_.isEmpty(domNode.children)) {
                     return (
-                        <ScriptTag key={index} {...node.attribs}>
-                            {_.map(node.children, childNode => convertNodeToElement(childNode, index, _.noop()))}
+                        <ScriptTag {...attrs}>
+                            {domToReact(domNode.children, options)}
                         </ScriptTag>
                     );
-                } else {
-                    return <ScriptTag key={index} {...node.attribs}/>;
                 }
+                return <ScriptTag {...attrs} />;
             }
         }
-    });
+    };
+
+    return parse(html, options);
 };
